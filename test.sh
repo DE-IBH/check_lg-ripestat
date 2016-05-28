449c1113a654262a3e60816402dcf696d936a3fb
Thomas Liske <liske@ibh.de>
2016-05-28 11:12:07 +0200

    Add total path count performance data/thresholds.

--- test.sh
+++ test.sh
@@ -28,3 +28,6 @@ chkrun 2 ./check_lg-ripestat --asn=42 --prefix=2a01:7700::/32 --peerings=3320,20
 
 chkrun 1 ./check_lg-ripestat --asn=15372 --prefix=212.111.224.0/19 --peerings=3320,13237,20676 -w 500:,,
 chkrun 2 ./check_lg-ripestat --asn=15372 --prefix=2a01:7700::/32 --peerings=3320,13237,20676 -c 500:
+
+chkrun 1 ./check_lg-ripestat --asn=15372 --prefix=212.111.224.0/19 --peerings=3320,13237,20676 --pw=500:
+chkrun 2 ./check_lg-ripestat --asn=15372 --prefix=212.111.224.0/19 --peerings=3320,13237,20676 --pc=500:
