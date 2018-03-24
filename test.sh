b43a503d957ce189d7b01be85c01108b361c5e62
Thomas Liske <thomas@fiasko-nw.net>
2018-03-24 16:56:06 +0100

    Update tests to match new peerings of AS15372.

--- test.sh
+++ test.sh
@@ -17,17 +17,17 @@ function chkrun {
     echo
 }
 
-chkrun 0 ./check_lg-ripestat --asn=15372 --prefix=212.111.224.0/19 --peerings=3320,13237,20676
-chkrun 0 ./check_lg-ripestat --asn=15372 --prefix=2a01:7700::/32 --peerings=3320,13237,20676
+chkrun 0 ./check_lg-ripestat --asn=15372 --prefix=212.111.224.0/19 --peerings=3320,13237,20676,174
+chkrun 0 ./check_lg-ripestat --asn=15372 --prefix=2a01:7700::/32 --peerings=3320,13237,20676,174
 
-chkrun 2 ./check_lg-ripestat --asn=15372 --prefix=212.111.224.0/19 --peerings=3320,20676
-chkrun 2 ./check_lg-ripestat --asn=15372 --prefix=2a01:7700::/32 --peerings=3320,20676
+chkrun 2 ./check_lg-ripestat --asn=15372 --prefix=212.111.224.0/19 --peerings=3320,20676,174
+chkrun 2 ./check_lg-ripestat --asn=15372 --prefix=2a01:7700::/32 --peerings=3320,20676,174
 
-chkrun 2 ./check_lg-ripestat --asn=42 --prefix=212.111.224.0/19 --peerings=3320,20676
-chkrun 2 ./check_lg-ripestat --asn=42 --prefix=2a01:7700::/32 --peerings=3320,20676
+chkrun 2 ./check_lg-ripestat --asn=42 --prefix=212.111.224.0/19 --peerings=3320,20676,174
+chkrun 2 ./check_lg-ripestat --asn=42 --prefix=2a01:7700::/32 --peerings=3320,20676,174
 
-chkrun 1 ./check_lg-ripestat --asn=15372 --prefix=212.111.224.0/19 --peerings=3320,13237,20676 -w 500:,,
-chkrun 2 ./check_lg-ripestat --asn=15372 --prefix=2a01:7700::/32 --peerings=3320,13237,20676 -c 500:
+chkrun 1 ./check_lg-ripestat --asn=15372 --prefix=212.111.224.0/19 --peerings=3320,13237,20676,174 -w 500:,,
+chkrun 2 ./check_lg-ripestat --asn=15372 --prefix=2a01:7700::/32 --peerings=3320,13237,20676,174 -c 500:
 
-chkrun 1 ./check_lg-ripestat --asn=15372 --prefix=212.111.224.0/19 --peerings=3320,13237,20676 --pw=500:
-chkrun 2 ./check_lg-ripestat --asn=15372 --prefix=212.111.224.0/19 --peerings=3320,13237,20676 --pc=500:
+chkrun 1 ./check_lg-ripestat --asn=15372 --prefix=212.111.224.0/19 --peerings=3320,13237,20676,174 --pw=500:
+chkrun 2 ./check_lg-ripestat --asn=15372 --prefix=212.111.224.0/19 --peerings=3320,13237,20676,174 --pc=500:
